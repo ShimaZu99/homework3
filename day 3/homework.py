4804411f304e948d0efcdf7f9957b4cf9a508dd4
ShimaZu99 <nikakhubua28@gmail.com>
2023-03-26 11:16:08 -0700

    day3 codes commit

---
+++ day 3/homework.py
@@ -0,0 +1,10 @@
+num1 = int(input("Chaweret Ricxvi: "))
+num2 = int(input("Chaweret Ricxvi: "))
+num3 = int(input("Chaweret Ricxvi: "))
+
+odd_numbers = (num1 + num2 + num3)
+if odd_numbers%2!=0:
+    print(odd_numbers)
+
+else:
+    print("tqven waaget")
